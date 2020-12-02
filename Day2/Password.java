b1606778e5fb1b0cdaf97b4a4b4111b896f6c76e
ericliu0000 <55063842+ericliu0000@users.noreply.github.com>
2020-12-02 11:08:46 -0500

    day 2 stuff
    no efficiency or comments, but i will fix that Soon

---
+++ Day2/Password.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Password {
+    public static void main(String[] args) {
+        File file = new File("input.txt");
+        ArrayList<String> text = new ArrayList<>();
+        String temp, value;
+        char letter;
+        int correctPasswords = 0;
+        int min, max;
+
+        // read the file things but safely
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while ((temp = reader.readLine()) != null) {
+                text.add(temp);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (String x : text) {
+            int letterCount = 0;
+            String[] spaces = x.split(" ");
+            min = Integer.parseInt(spaces[0].split("-")[0]);
+            max = Integer.parseInt(spaces[0].split("-")[1]);
+            
+            letter = spaces[1].toCharArray()[0];
+            value = spaces[2];
+
+            for (char i : value.toCharArray()) {
+                if (i == letter) letterCount++;
+            }
+
+            if ((min <= letterCount) && (letterCount <= max)) correctPasswords++;
+        }
+
+        System.out.println(correctPasswords);
+        // System.out.println(text);
+    }
+}
---
+++ Day2/PasswordAgain.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class PasswordAgain {
+    public static void main(String[] args) {
+        File file = new File("input.txt");
+        ArrayList<String> text = new ArrayList<>();
+        String temp;
+
+        char letter;
+        char[] value;
+
+        int correctPasswords = 0;
+        int i1, i2;
+
+        // read the file things but safely
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while ((temp = reader.readLine()) != null) {
+                text.add(temp);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (String x : text) {
+            String[] spaces = x.split(" ");
+            i1 = Integer.parseInt(spaces[0].split("-")[0]);
+            i2 = Integer.parseInt(spaces[0].split("-")[1]);
+
+            letter = spaces[1].toCharArray()[0];
+            value = spaces[2].toCharArray();
+
+            if (((value[i1 - 1]) == letter) != ((value[i2 - 1]) == letter)) {
+                correctPasswords++;
+                System.out.println("\n\n");
+                System.out.println((value[i1 - 1]));
+                // System.out.println(letter);
+                // System.out.println(i1);
+                // System.out.println(i2);
+                System.out.println(value);
+            }
+        }
+
+        System.out.println(correctPasswords);
+        // System.out.println(text);
+    }
+}
